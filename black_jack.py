abc3719bde9971df9cc84ffe119792a21bbcfcfa
serj <serjst@icloud.com>
2021-04-11 09:17:02 +0300

    v 0.0.0.0.0.0.0.0.0.1

--- black_jack.py
+++ black_jack.py
@@ -117,22 +117,26 @@ class DeckOfCards:
 
 
 class Player:
-    hand = []
-    # def score(self):
-    #     return self.score
 
-    def __init__(self, name, score=0):
+    def __init__(self, name, hand=None, score=0):
         self.name = name
         self.score = score
+        if hand is None:
+            hand = []
+        self.hand = hand
 
     def sum_score(self):
-
+        self.score = 0
         for i in self.hand:
+            # print('card is ', i.value)
             self.score = self.score + i.value
+            # print("Inside ",self.score)
+        # print("Out ", self.score)
+
         return self.score
 
-    def take_card(self, card):
-        self.hand.append(card.pop(0))
+    def take_card(self, shoe):
+        self.hand.append(shoe.pop(0))
 
 
 class Croupier(Player):
@@ -171,13 +175,10 @@ deck = DeckOfCards.full_deck
 # player1.take_card(shoe)
 # player1.take_card(shoe)
 # crup.take_card(shoe)
-#
-#
-# print(player1.hand, player1.sum_score())
 
 
 def game():
-    name = input("Enter ur name")
+    name = input("Enter ur name ")
     player = Player(name)
 
     crup = Croupier()
@@ -185,22 +186,27 @@ def game():
     game_loop = True
 
     while game_loop:
-        a = input("PRESS A TO TAKE A CARD")
-        if a == 'a'.lower():
-            player.take_card(shoe)
-            crup.take_card(shoe)
-            print('Crup took a card')
-            print(f"ur cards  {player.hand}")
-            print(f'{crup.hand} is CRUP')
-            a2 = input("Wanna more? press a to take more or press any key if its enough")
-            if a2 == "a":
-                continue
-            else:
-                if player.sum_score() <= 21 and player.sum_score() > crup.sum_score():
-                    print("you WIN")
+        if player.sum_score() < 21:
+            a = input("PRESS A TO TAKE A CARD")
+            if a == 'a'.lower():
+                player.take_card(shoe)
+                crup.take_card(shoe)
+                print('Crup took a card')
+                print(f"ur cards  {player.hand} (sum = {player.sum_score()})")
+                print(f'{crup.hand} is CRUP   (sum = {crup.sum_score()})')
+                a2 = input("Wanna more? press a to take more or press any key if its enough")
+                if a2 == "a":
+                    continue
                 else:
-                    print('LOOSEER')
-
+                    if player.sum_score() <= 21 and player.sum_score() > crup.sum_score():
+                        print(f"you WIN Ur score is {player.sum_score()} , crup {crup.sum_score}")
+                        break
+                    else:
+                        print(f'LOOSEER Ur score is {player.sum_score()} , crup {crup.sum_score}')
+                        break
+        else:
+            print('LOST SCORE is > 21')
+            break
 
 game()
 
