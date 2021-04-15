62f6aa2b06d54e026bb1ee7fc14d4e0a6e7ce5b6
serj <serjst@icloud.com>
2021-04-15 15:19:17 +0300

    v 0.0.0.0.0.0.0.2

--- black_jack.py
+++ black_jack.py
@@ -1,4 +1,3 @@
-from random import sample
 import pygame
 # from pygame.sprite import RenderUpdates
 # from pygame.locals import *
@@ -6,6 +5,7 @@ import sys
 import os
 # from UIElement import UIElement
 from cards import DeckOfCards, Hand
+from players import Player, Croupier
 
 pygame.init()
 fps = 30
@@ -56,49 +56,6 @@ def sum_cards(cards):
     for card in cards:
         score = score + card.value
     return score
-
-
-class Player:
-
-    def __init__(self, name, hand=None, score=0):
-        self.name = name
-        self.score = score
-        if hand is None:
-            hand = []
-        self.hand = hand
-
-    def sum_score(self):
-        self.score = 0
-        for i in self.hand:
-            # print('card is ', i.value)
-            self.score = self.score + i.value
-            # print("Inside ",self.score)
-        # print("Out ", self.score)
-
-        return self.score
-
-    def take_card(self, shoe):
-        self.hand.append(shoe.pop(0))
-
-
-class Croupier(Player):
-
-    shoe = []
-
-    def __init__(self):
-        super().__init__(name="Croupier")
-
-    def make_a_shoe(self):
-        # Почему то при создании нового крупье, перемешанная коложа такая же как и у первого
-        shuffled_deck = sample(DeckOfCards.full_deck, len(DeckOfCards.full_deck))
-
-        for card in shuffled_deck:
-            self.shoe.append(card)
-
-        return self.shoe
-
-
-
 
 
 def game():
---
+++ players.py
@@ -0,0 +1,43 @@
+from random import sample
+from cards import DeckOfCards
+
+
+class Player:
+
+    def __init__(self, name, hand=None, score=0):
+        self.name = name
+        self.score = score
+        if hand is None:
+            hand = []
+        self.hand = hand
+
+    def sum_score(self):
+        self.score = 0
+        for i in self.hand:
+            # print('card is ', i.value)
+            self.score = self.score + i.value
+            # print("Inside ",self.score)
+        # print("Out ", self.score)
+
+        return self.score
+
+    def take_card(self, shoe):
+        self.hand.append(shoe.pop(0))
+
+
+class Croupier(Player):
+
+    shoe = []
+
+    def __init__(self):
+        super().__init__(name="Croupier")
+
+    def make_a_shoe(self):
+        # Почему то при создании нового крупье, перемешанная коложа такая же как и у первого
+        shuffled_deck = sample(DeckOfCards.full_deck, len(DeckOfCards.full_deck))
+
+        for card in shuffled_deck:
+            self.shoe.append(card)
+
+        return self.shoe
+
